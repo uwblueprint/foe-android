55aff5ea9c5d287e08c4b473adcba6aa5659d79b
John Salaveria <jj.salaveria@gmail.com>
2017-07-16 18:55:59 -0400

    Learn More, first build

--- BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/LearnActivity.java
+++ BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/LearnActivity.java
@@ -1,11 +1,14 @@
 package com.blueprint.foe.beetracker;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.blueprint.foe.beetracker.Model.FactCollection;
@@ -22,33 +25,17 @@ public class LearnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
 
-        facts.addFact("Don\'t Mow So Low", "Mow so low.");
-        facts.addFact("Stop Sprinkler Irrigation", "Stop sprinkler irrigation because it's bad.");
+        facts.addFact("Don\'t Mow So Low",
+                "Mow lawns with a high blade setting to allow violets and clovers to grow.");
+        facts.addFact("Stop Sprinkler Irrigation",
+                "Bees can perceive imminent rain and will seak protection in their nests. Sprinkler irrigation offers no warning cues and can alter visual landmarks and entrance to nests.",
+                FactCollection.Category.WATER);
+        facts.addFact("Stop Fertilizing",
+                "Native plants do not need it, and fertilizing only encourages weeds and invasive species.");
 
         final FactsAdapter adapter = new FactsAdapter(this, facts.getFacts());
         ListView listView = (ListView) findViewById(R.id.factsListView);
 
         listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick( AdapterView<?> parent, View view, final int position, long id) {
-                LinearLayout completeButton = (LinearLayout) view.findViewById(R.id.btnCompleteFact);
-
-                completeButton.setOnClickListener( new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        System.out.println( position + " clicked!");
-                        facts.getFacts().remove(position);
-
-                        adapter.notifyDataSetChanged();
-
-                    }
-
-                });
-            }
-        });
-
     };
 }
--- BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/Model/FactCollection.java
+++ BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/Model/FactCollection.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 /**
  * Created by johnsington on 2017-07-07.
  */
+
 public class FactCollection {
+
+    public enum Category {
+        GENERAL, GARDEN, WATER
+    }
 
     ArrayList<Fact> facts;
     static int id;
@@ -18,6 +23,13 @@ public class FactCollection {
     public void addFact(String title, String description) {
         Fact newFact = new Fact(title, description, this.id);
         this.facts.add(newFact);
+        this.id++;
+    }
+
+    public void addFact(String title, String description, Category category) {
+        Fact newFact = new Fact(title, description, this.id, category);
+        this.facts.add(newFact);
+        this.id++;
     }
 
     public ArrayList<Fact> getFacts () {
@@ -25,22 +37,37 @@ public class FactCollection {
     }
 
     public class Fact {
+
         String title;
         String description;
+        Category category;
         int id;
+
 
         public Fact(String title, String description, int id) {
             this.title = title;
             this.description = description;
             this.id = id;
+            this.category = Category.GENERAL;
+        }
+
+        public Fact(String title, String description, int id, Category category) {
+            this(title, description, id);
+            this.category = category;
         }
 
         public String getTitle() {
             return this.title;
         }
 
+        public int getId() {
+            return this.id;
+        }
+
         public String getDescription() {
             return this.description;
         }
+
+        public Category getCategory() { return this.category; }
     }
 }
