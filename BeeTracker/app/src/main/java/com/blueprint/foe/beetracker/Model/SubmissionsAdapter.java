4bcbef3b42628208582d30c81f0ca3619f884421
Luisa San Martin <luisa2000@gmail.com>
2018-07-05 11:30:33 -0400

    Finished modal

--- BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/Model/SubmissionsAdapter.java
+++ BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/Model/SubmissionsAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +19,7 @@ import com.blueprint.foe.beetracker.API.BeeTrackerCaller;
 import com.blueprint.foe.beetracker.R;
 import com.blueprint.foe.beetracker.SightingFragment;
 import com.blueprint.foe.beetracker.SignUpFragment;
+import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
@@ -28,10 +30,10 @@ import java.util.Locale;
 /**
  * An ArrayAdapter for the submission collection. Used by HistoryActivity.
  */
-public class SubmissionsAdapter extends ArrayAdapter<BeeTrackerCaller.SubmissionResponse> {
+public class SubmissionsAdapter extends ArrayAdapter<CompletedSubmission> {
     private static String TAG = SubmissionsAdapter.class.toString();
 
-    public SubmissionsAdapter(Context context, BeeTrackerCaller.SubmissionResponse[] submissions) {
+    public SubmissionsAdapter(Context context, CompletedSubmission[] submissions) {
         super(context, 0, submissions);
     }
 
@@ -56,12 +58,19 @@ public class SubmissionsAdapter extends ArrayAdapter<BeeTrackerCaller.Submission
 
                 });
 
+        final CompletedSubmission submission = getItem(position);
+
         LinearLayout container = (LinearLayout) convertView.findViewById(R.id.submissionViewContainer);
         container.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Gson gson = new Gson();
+                String submissionString = gson.toJson(submission);
+                bundle.putString(SightingFragment.SIGHTING_KEY, submissionString);
                 FragmentTransaction fragmentTransaction = ((Activity) getContext()).getFragmentManager().beginTransaction();
                 SightingFragment sightingFragment = new SightingFragment();
+                sightingFragment.setArguments(bundle);
                 fragmentTransaction.replace(R.id.history_container_layout, sightingFragment);
                 fragmentTransaction.addToBackStack("main");
                 fragmentTransaction.commit();
@@ -70,28 +79,23 @@ public class SubmissionsAdapter extends ArrayAdapter<BeeTrackerCaller.Submission
 
         ImageView gradient = (ImageView) convertView.findViewById(R.id.imgGradient);
         gradient.setImageDrawable(gradientDrawable);
-        try {
-            final CompletedSubmission submission = getItem(position).getSubmission();
 
-            TextView submissionDate = (TextView) convertView.findViewById(R.id.tvDate);
-            SimpleDateFormat formatter = new SimpleDateFormat("MMMM d", Locale.ENGLISH);
-            String formatted = formatter.format(submission.getDate());
-            submissionDate.setText(formatted);
-            TextView submissionSpecies = (TextView) convertView.findViewById(R.id.tvSpecies);
-            if (submission.getSpecies() != null) {
-                submissionSpecies.setText(submission.getSpecies().getEnglishName());
-            }
-            TextView submissionAddress = (TextView) convertView.findViewById(R.id.tvLocation);
-            submissionAddress.setText(submission.getStreetAddress());
-
-            ImageView imageView = (ImageView) convertView.findViewById(R.id.imgSubmission);
-            Picasso.with(getContext())
-                    .load(submission.getImageUrl())
-                    .placeholder(R.mipmap.submission_placeholder)
-                    .into(imageView);
-        } catch(ParseException e) {
-            Log.e(TAG, e.toString());
+        TextView submissionDate = (TextView) convertView.findViewById(R.id.tvDate);
+        SimpleDateFormat formatter = new SimpleDateFormat("MMMM d", Locale.ENGLISH);
+        String formatted = formatter.format(submission.getDate());
+        submissionDate.setText(formatted);
+        TextView submissionSpecies = (TextView) convertView.findViewById(R.id.tvSpecies);
+        if (submission.getSpecies() != null) {
+            submissionSpecies.setText(submission.getSpecies().getEnglishName());
         }
+        TextView submissionAddress = (TextView) convertView.findViewById(R.id.tvLocation);
+        submissionAddress.setText(submission.getStreetAddress());
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imgSubmission);
+        Picasso.with(getContext())
+                .load(submission.getImageUrl())
+                .placeholder(R.mipmap.submission_placeholder)
+                .into(imageView);
         return convertView;
     }
 }
--- BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/SightingFragment.java
+++ BeeTracker/app/src/main/java/com/blueprint/foe/beetracker/SightingFragment.java
@@ -1,18 +1,101 @@
 package com.blueprint.foe.beetracker;
 
 import android.app.Fragment;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blueprint.foe.beetracker.Model.CompletedSubmission;
+import com.blueprint.foe.beetracker.Model.CurrentSubmission;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
- * Created by luisa on 2018-07-04.
+ * Shows the details of a specific sighting.
  */
 public class SightingFragment extends Fragment {
+    public final static String SIGHTING_KEY = "SightingKey";
+
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.sighting_fragment, container, false);
+
+        if (getArguments() == null) {
+            // ERROR
+            getFragmentManager().popBackStack();
+            Toast.makeText(getActivity(), "There was an error loading your sighting.", Toast.LENGTH_LONG).show();
+            return view;
+        }
+
+        String submissionString = getArguments().getString(SIGHTING_KEY);
+        Gson gson = new Gson();
+        CompletedSubmission submission = gson.fromJson(submissionString, CompletedSubmission.class);
+
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                new int[]{ContextCompat.getColor(getActivity(), R.color.gradient1),
+                        ContextCompat.getColor(getActivity(), R.color.gradient2),
+                        ContextCompat.getColor(getActivity(), R.color.gradient3),
+                        ContextCompat.getColor(getActivity(), R.color.gradient4),
+                        ContextCompat.getColor(getActivity(), R.color.gradient5),
+                        ContextCompat.getColor(getActivity(), R.color.gradient6),
+                        ContextCompat.getColor(getActivity(), R.color.gradient7),
+                        ContextCompat.getColor(getActivity(), R.color.gradient8)
+
+                });
+
+        ImageView gradient = (ImageView) view.findViewById(R.id.imgGradient);
+        gradient.setImageDrawable(gradientDrawable);
+
+        ImageView capturedImage = (ImageView) view.findViewById(R.id.ivBee);
+        Picasso.with(getActivity())
+                .load(submission.getImageUrl())
+                .placeholder(R.mipmap.submission_placeholder)
+                .into(capturedImage);
+
+        TextView date = (TextView) view.findViewById(R.id.tvDate);
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE, MMMM d", Locale.ENGLISH);
+        String formatted = formatter.format(submission.getDate());
+        date.setText(formatted);
+
+        TextView englishSpecies = (TextView) view.findViewById(R.id.species_english);
+        ImageView beeSpecies = (ImageView) view.findViewById(R.id.ivBeeSpecies);
+        TextView latinSpecies = (TextView) view.findViewById(R.id.species_latin);
+        if (submission.getSpecies() != null) {
+            englishSpecies.setText(submission.getSpecies().getEnglishName());
+            latinSpecies.setText(getString(R.string.latin_species, submission.getSpecies().toString()));
+            beeSpecies.setImageResource(submission.getSpecies().getResource());
+        }
+
+        TextView location = (TextView) view.findViewById(R.id.location);
+        location.setText(submission.getStreetAddress());
+
+        ImageView weatherIcon = (ImageView) view.findViewById(R.id.weather_icon);
+        weatherIcon.setImageResource(submission.getWeather().getResource());
+
+        TextView weatherText = (TextView) view.findViewById(R.id.weather_status);
+        weatherText.setText(submission.getWeather().toString());
+
+        TextView habitatText = (TextView) view.findViewById(R.id.habitat_status);
+        habitatText.setText(submission.getHabitat().toString());
+
+        CardView closeButton = (CardView) view.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().popBackStack();
+            }
+        });
 
         return view;
     }
